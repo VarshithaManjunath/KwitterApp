85328693972f8796c19d709317eac5a3395f9c5c
VARSHITHA M <45088568+varshithamanasa@users.noreply.github.com>
2021-09-18 13:23:20 +0530

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -2,6 +2,9 @@
 function addUser() {
 
   user_name = document.getElementById("user_name").value;
+   firebase.database().ref("/").child(user_name).update({
+    purpose : "adding room name"
+  });
 
   localStorage.setItem("user_name", user_name);
   
